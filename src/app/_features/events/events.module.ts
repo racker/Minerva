06d2d8b9c189a5e05deb4d014fe2200d9ba356da
Lovedeep Singh <love7875@mcce3zmd6m.bbrouter>
2020-08-12 00:03:38 +0530

    add shared module inside event module

--- src/app/_features/events/events.module.ts
+++ src/app/_features/events/events.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EventsService } from '../../_services/events/events.service';
+import { SharedModule } from '../../_shared/shared.module';
+
 
 @NgModule({
   declarations: [],
   imports: [
+    SharedModule,
     CommonModule
   ],
   providers:[
