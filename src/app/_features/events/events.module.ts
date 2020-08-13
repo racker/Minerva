32ecc55bac7f46c71498d551dede108390a094f7
prashant mishra <prashantmishra@prashants-MacBook-Pro.local>
2020-08-13 18:37:55 +0530

    route added

--- src/app/_features/events/events.module.ts
+++ src/app/_features/events/events.module.ts
@@ -3,14 +3,22 @@ import { CommonModule } from '@angular/common';
 import { EventsService } from '../../_services/events/events.service';
 import { SharedModule } from '../../_shared/shared.module';
 import { EventDetailsComponent } from '../events/components/event-details/event-details.component';
-import { RouterModule } from '@angular/router';
-import { routes } from "./events.route";
+import { RouterModule, Routes } from '@angular/router';
 
+
+ const routes: Routes = [
+  {
+      path: '',
+      component: EventDetailsComponent,
+      data: {
+        breadcrumb: ''
+      }
+  }
+];
 @NgModule({
   declarations: [EventDetailsComponent],
   imports: [
     SharedModule,
-    CommonModule,
     RouterModule.forChild(routes)
   ],
   providers:[
