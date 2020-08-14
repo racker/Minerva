c5459a856189e571017c7a6281327e52504dc638
prashant mishra <prashant.mishra@rackspace.com>
2020-08-14 19:22:24 +0530

    mocking

--- src/app/_features/events/components/event-details/event-details.component.ts
+++ src/app/_features/events/components/event-details/event-details.component.ts
@@ -1,24 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { EventsService } from "../../../../_services/events/events.service";
-
+import { SpinnerService } from 'src/app/_services/spinner/spinner.service';
+import { tap } from 'rxjs/operators';
+declare const window: any;
 @Component({
   selector: 'app-event-details',
   templateUrl: './event-details.component.html',
   styleUrls: ['./event-details.component.scss']
 })
 export class EventDetailsComponent implements OnInit {
-  event:any;
-  constructor(private eventSerice: EventsService) { }
+  $evnt:any;
+  Object = window.Object;
+  constructor(private eventSerice: EventsService, private spnService: SpinnerService) {
+    this.spnService.changeLoadingStatus(true);
+   }
 
   ngOnInit(): void {
     this.getEvent();
   }
 
   getEvent() {
-    this.eventSerice.getEventById("1e2434f5-439d-42b5-aa3b-870aefd1b1e1").subscribe(data => {
-      this.event= data;
-      console.log(data);
-    })
+    this.$evnt=this.eventSerice.getEventById("1e2434f5-439d-42b5-aa3b-870aefd1b1e1").pipe(
+      tap((data)=>{
+        this.spnService.changeLoadingStatus(false);
+        console.log(data);
+      })
+    )
+   
   }
 
   
--- src/app/_services/events/events.service.ts
+++ src/app/_services/events/events.service.ts
@@ -1,43 +1,50 @@
 import { Injectable } from '@angular/core';
-import { HttpClient,HttpHeaders } from "@angular/common/http";
-import { environment} from "../../../environments/environment";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { environment } from "../../../environments/environment";
 import { Event } from "../../_models/events";
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { LoggingService } from '../logging/logging.service';
 import { LogLevels } from 'src/app/_enums/log-levels.enum';
+import { EventsMock } from "../../_mocks/events/events.service.mock";
 
 const httpOption = {
   headers: new HttpHeaders({
-     'Content-type':'application/json'
+    'Content-type': 'application/json'
   })
 }
 @Injectable({
   providedIn: 'root'
 })
 export class EventsService {
+  private eventMock= new EventsMock();
 
   private event;
 
-  
+
   public get getEvent() {
     return this.event
   }
-  
-  public set setEvent(v : any) {
+
+  public set setEvent(v: any) {
     this.event = v;
   }
-  
-  constructor(private http: HttpClient,private logService: LoggingService) { }
 
-  getEventById(id):Observable<Event>{
-    return this.http.get<Event>(`${environment.api.salus}/event-tasks/${id}`,httpOption)
-    .pipe(
-      tap(data =>{
-        this.setEvent=data;
-        this.logService.log(this.event, LogLevels.info);
-      })
-    )
+  constructor(private http: HttpClient, private logService: LoggingService) { }
+
+  getEventById(id): Observable<Event> {
+    if (environment.mock) {
+      let mock = Object.assign({},this.eventMock.single)
+      this.event= mock;
+      return of<Event>(this.event);
+    }
+    return this.http.get<Event>(`${environment.api.salus}/event-tasks/${id}`, httpOption)
+      .pipe(
+        tap(data => {
+          this.setEvent = data;
+          this.logService.log(this.event, LogLevels.info);
+        })
+      )
   }
-  
+
 }
