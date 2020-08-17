9cc6d5ea7eb0437957d5fcb1e0343cd8509e2000
Lovedeep Singh <lovedeep.singh@rackspace.com>
2020-08-17 19:08:41 +0530

    added test case for event service

--- src/app/_services/events/events.service.spec.ts
+++ src/app/_services/events/events.service.spec.ts
@@ -1,5 +1,7 @@
 import { TestBed } from '@angular/core/testing';
-
+import { of } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { EventsMock } from '../../_mocks/events/events.service.mock';
 import { EventsService } from './events.service';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -17,5 +19,16 @@ describe('EventsService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+
+  it('should return all events', () => {
+    service.getEvents(environment.pagination.pageSize).subscribe((data) => {
+      let mocked = new EventsMock().eventList;
+      let slicedArray = new EventsMock().eventList.content
+       .slice(0 * environment.pagination.pageSize, 1 * environment.pagination.pageSize);
+      mocked.content = slicedArray
+      expect(data).toEqual(mocked);
+    });
   });
 });
