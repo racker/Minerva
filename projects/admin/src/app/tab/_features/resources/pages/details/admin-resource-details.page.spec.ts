bab3f76ddfd8d70814e15470b3ba2c85907d0c4d
Prashant Mishra <pras7430@prashants-MacBook-Pro.local>
2020-09-18 20:36:53 +0530

    added test cases

--- projects/admin/src/app/tab/_features/resources/pages/details/admin-resource-details.page.spec.ts
+++ projects/admin/src/app/tab/_features/resources/pages/details/admin-resource-details.page.spec.ts
@@ -1,7 +1,7 @@
 import { async, ComponentFixture, TestBed, getTestBed } from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterTestingModule } from '@angular/router/testing';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpErrorResponse } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
 import { ResourcesPage } from '../../../../../../../../../src/app/_features/resources/pages/resources/resources.page';
 
@@ -10,28 +10,119 @@ import { ResourcesListComponent } from '../../../../../../../../../src/app/_feat
 import { resourcesMock } from '../../../../../../../../../src/app/_mocks/resources/resources.service.mock';
 import { SharedModule } from '../../../../../../../../../src/app/_shared/shared.module';
 import { ResourcesService } from 'src/app/_services/resources/resources.service';
-import { of, Observable } from 'rxjs';
+import { of, Observable, throwError, Subscriber } from 'rxjs';
+import { CreateResource, Resource, Resources } from 'src/app/_models/resources';
+import { delay } from 'rxjs/operators';
+import { By } from '@angular/platform-browser';
+
+
+
+class ResourcesServiceMock {
+  
+  private _resources: Resources;
+  private _resource: Resource;
+
+  private mockedResources = new resourcesMock();
+  get resources(): Resources {
+    return this._resources;
+  }
+
+  set resources(value: Resources) {
+    this._resources = value;
+  }
+
+  get resource(): Resource {
+    return this._resource;
+  }
+
+  set resource(value: Resource) {
+    this._resource = value;
+  }
+
+  /**
+   * Gets a list of Resources
+   * @param size
+   * @param page
+   * @returns Observable<Resources>
+   */
+  getResources(size?: number, page?: number): Observable<Resources> {
+      let mocks = Object.assign({}, this.mockedResources.collection);
+      let slicedData = [... mocks.content.slice(page * size, (page + 1) * size)];
+      this.resources = mocks;
+      this.resources.content = slicedData;
+      return of<Resources>(this.resources).pipe(delay(500));
+  }
+
+  /**
+   * Gets a single Resource
+   * @param id
+   * @returns Observable<Resource>
+   */
+  getResource(id: string): Observable<Resource> {
+      this._resource = this.mockedResources.single;
+      return of<Resource>(this.mockedResources.single).pipe(delay(500));
+  }
+
+  /**
+   * @description Creates a resource with preliminary resource object
+   * @param resource CreateResource
+   * @returns Resource
+   */
+  createResource(resource:CreateResource): Observable<Resource> {
+      this._resource = this.mockedResources.single;
+      return of<Resource>(this.mockedResources.single);
+  }
+
+  /**
+   * Updates a resource
+   * @param id string
+   * @param updatedData {[key: string]: any}
+   * @returns Observable<Resource>
+   */
+  updateResource(id:string, updatedData: {[key: string]: any}): Observable<Resource> {
+      this._resource = this.mockedResources.single
+      return of<Resource>(this.mockedResources.single);
+  }
+
+  /**
+   * @description Validates that the resourceId being created isn't alreay in use
+   * by the tenant, as these must be unique on a per tenant basis
+   * @param id string
+   * @returns HttpResponse of empty object OR a boolean when in offline mode
+    */
+  validateResourceId(id:string): any {
+      return throwError(new HttpErrorResponse({
+        error: 'Not Found',
+        status: 404
+      }));
+    
+  }
+
+  searchResources(search:string): Observable<Resources> {
+      let mocks = Object.assign({}, this.mockedResources.collection);
+      this.resources = mocks;
+      let slicedData = [... mocks.content.slice(0 * 10, 1 * 10)];
+      this.resources.content = slicedData;
+      return of<Resources>(this.resources);
+  }
+
+  /**
+   * @description
+   * @param id string
+   */
+  deleteResource(id:string) {
+      return of<boolean>(true);
+  }
+}
+
 
 const routes = [
   {
-    path: 'resources',
+    path: 'resources/:id',
+    component: AdminResourceDetailsPage,
     data: {
-      breadcrumb: 'RESOURCES'
-    },
-    children: [{
-      path: '',
-      component: ResourcesPage,
-      data: {
-        breadcrumb: ''
-      }
-    },
-    {
-      path: ':id',
-      component: AdminResourceDetailsPage,
-      data: {
-        breadcrumb: 'DETAILS'
-      }
-    }]
+      breadcrumb: ''
+    }
   }
 ];
 
@@ -91,25 +182,27 @@ describe('AdminResourceDetailsPage', () => {
       imports: [
         SharedModule,
         HttpClientModule,
-        RouterTestingModule
+        RouterTestingModule.withRoutes([
+          {path:'resources',component:ResourcesPage}
+        ])
       ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    injector = getTestBed();
-    // TestBed.overrideComponent(AdminResourceDetailsPage,{
-    //   set:{
-    //     providers:[{
-          
-    //     }]
-    //   }
-    // })
+    TestBed.overrideComponent(AdminResourceDetailsPage,{
+      set:{
+        providers:[{
+          provide: ResourcesService , useClass:ResourcesServiceMock
+        }]
+      }
+    })
     fixture = TestBed.createComponent(AdminResourceDetailsPage);
     component = fixture.componentInstance;
-    resourceService = injector.get(ResourcesService);
+    resourceService = fixture.debugElement.injector.get(ResourcesService);
     fixture.detectChanges();
+    
   });
 
   it('should create', () => {
@@ -125,11 +218,10 @@ describe('AdminResourceDetailsPage', () => {
     expect(component.labelsLoading).toEqual(false);
     expect(component.deleteLoading).toEqual(false);
     expect(component.message).toEqual("Are you sure you'd like to delete this Resource?");
-    expect(component.modalType).toEqual('delResModal');
-    expect(component.metaPopPencil).toBeDefined();
-    expect(component.labelPopPencil).toBeDefined();
-    expect(component.delResource).toBeDefined();
-    expect(component.subManager).toBeDefined();
+    // expect(component.metaPopPencil).toBeDefined();
+    // expect(component.labelPopPencil).toBeDefined();
+    // expect(component.delResource).toBeDefined();
+    // expect(component.subManager).toBeDefined();
   });
 
   it('should have a route param', () => {
@@ -141,11 +233,26 @@ describe('AdminResourceDetailsPage', () => {
     expect(component.updatedMetaFields).toEqual(formattedKeyPair);
   });
 
-  it('should finalize update of meta values finalizeMeta()', (done) => {
+  it('should finalize update of meta values finalizeMeta(), when metaform and label form not submited not valid', (done) => {
 
     let spy = spyOn(resourceService, 'updateResource')
     .and.returnValue(of(new resourcesMock().single));;
     resourceService.resource = new resourcesMock().single;
+    component.metaFormSubmit.next(false);
+    component.labelFormSubmit.next(false);
+    component.metaValueUpdated(keyPair);
+    component.finalizeMeta();
+    expect(spy).toHaveBeenCalled();
+    done();
+  });
+
+  it('should finalize update of meta values finalizeMeta(), when metaform and label for valid submited valid', (done) => {
+
+    let spy = spyOn(resourceService, 'updateResource')
+    .and.returnValue(of(new resourcesMock().single));;
+    resourceService.resource = new resourcesMock().single;
+    component.metaFormSubmit.next(true);
+    component.labelFormSubmit.next(true)
     component.metaValueUpdated(keyPair);
     component.finalizeMeta();
     expect(spy).toHaveBeenCalled();
@@ -166,17 +273,28 @@ describe('AdminResourceDetailsPage', () => {
     done();
   });
 
-  it('should set to a single resource', () => {
+  it('should set to a single resource', (done) => {
     let mocked = new resourcesMock().single;
     component.resource$.subscribe((resource) => {
       expect(resource).toEqual(mocked);
+      done();
     });
   });
 
   it('should delete a resource', (done) => {
-    let spy = spyOn(resourceService, 'deleteResource').and.returnValue(of());
+    let spy = spyOn(resourceService, 'deleteResource').and.returnValue(of(true));
     component.triggerConfirm('resourceToDelete');
     expect(spy).toHaveBeenCalled();
     done();
   })
+  it('when fail to delete a resource', (done) => {
+    let spy = spyOn(resourceService, 'deleteResource').and.returnValue(throwError({status: 404}));
+    component.delResourcePop=fixture.debugElement.query(By.css('.nodisplaClick'));
+    component.delResource= fixture.debugElement.query(By.css('#delResLink'));
+    
+    component.triggerConfirm('resourceToDelete');
+    expect(spy).toHaveBeenCalled();
+    done();
+  })
+
 });
