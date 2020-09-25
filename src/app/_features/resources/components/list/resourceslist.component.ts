4eaaf23cdc7726d3e74c2b3f0bae9f1f334f291e
Lovedeep Singh <lovedeep.singh@rackspace.com>
2020-09-25 23:54:34 +0530

    make disable ok flag true

--- src/app/_features/resources/components/list/resourceslist.component.ts
+++ src/app/_features/resources/components/list/resourceslist.component.ts
@@ -220,6 +220,7 @@ export class ResourcesListComponent implements OnInit, OnDestroy {
     this.fetchResources();
     this.resourceArr    = [];
     this.resourceErrArr = [];
+    this.disableOk      = true;
   }
 
 
