36abd720de41c94e81c3d3e7fb32f81d2858f7d2
RiaRenold <ria.renold@gmail.com>
2020-10-07 13:57:16 +0530

    Adding testcases of DeleteMultipleMonitors-MNRVA-583

--- e2e/src/pages/monitorListPage.ts
+++ e2e/src/pages/monitorListPage.ts
@@ -1,12 +1,32 @@
-import { element, by } from "protractor"
+import { element, by, ElementFinder } from "protractor"
 
 
 export class MonitorsListPage
 {
-    pageTitle          =element(by.xpath("//h2[contains(text(),'Monitors')]"));
-    createMonitorBtn   =element(by.xpath("//button[@class='hxBtn hxPrimary' or contains(text(),'Create Monitor')]"));
+    pageTitle                  =element(by.xpath("//h2[contains(text(),'Monitors')]"));
+    createMonitorBtn            =element(by.xpath("//button[@class='hxBtn hxPrimary' or contains(text(),'Create Monitor')]"));
+    webtable                    =element(by.tagName('tbody'));
+    noOfRows                    =this.webtable.all(by.tagName('tr'));
+    checkBoxes                  =this.noOfRows.all(by.tagName('hx-checkbox'))  
+    deleteBtn                   =element(by.xpath("//hx-disclosure[contains(text(),'Delete')]"))
+    successListOfMonitors       =element.all(by.xpath("//hx-div[@class='hxDivSuccess']"))
 
-   paginationLastPageButton(){
+   
+    selectFiveMonitors(){
+        let count:number;
+        for(let i=1;i<=5;i++){
+           this.checkBoxes.get(i).click();
+           count=i;
+        }
+        return count
+   }
+
+   selectAParticularCheckBox(checkboxNo:number){
+        return(element(by.xpath('//tr['+checkboxNo+']//hx-checkbox')))
+   }
+   
+
+    paginationLastPageButton(){
         return(element(by.xpath("//button[@class='hxBtn lastPage']")));
     }
 
@@ -38,4 +58,6 @@ export class MonitorsListPage
         return(element(by.xpath("//hx-busy[@class='gbl-spinner-show']")));
         
     }
+
+
 }
