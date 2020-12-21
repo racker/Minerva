23faabcd60af158be41e5ec4eb153370bb55e23c
Lovedeep Singh <lovedeep.singh@rackspace.com>
2020-12-21 21:32:34 +0530

    add portalsessionid

--- proxy.stage.conf.js
+++ proxy.stage.conf.js
@@ -11,7 +11,7 @@
       "secure": false,
       "logLevel": "debug",
       "onProxyReq": function (proxyReq) {
-        proxyReq.setHeader('Cookie', `__Secure-portal_sessionid=${portalSessionId}`);
+        proxyReq.setHeader('Cookie', `__Secure-portal_sessionid=${headerObj.portalSessionId}`);
         proxyReq.setHeader('Referer', `https://staging.portal.rackspace.com/${headerObj.tenantId}/intelligence`)
       }
     },
