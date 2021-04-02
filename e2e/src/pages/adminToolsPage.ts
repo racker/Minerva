6ceb5def264305ee27464670fe3df62acd730279
Lovedeep Singh <lovedeep.singh@rackspace.com>
2021-04-02 20:53:07 +0530

    revert changes

--- e2e/src/pages/adminToolsPage.ts
+++ e2e/src/pages/adminToolsPage.ts
@@ -2,7 +2,7 @@ import { element, by, browser, protractor } from "protractor";
 
 export class AdminToolsPage{
     
-    accountSearchBox = element(by.id("demoSearch"));
+    accountSearchBox = element(by.xpath("//input[@placeholder='Account Search']"));
     monitorsTab      = element(by.xpath("//hx-tab[contains(text(),'Monitors')]"));
     resourcesTab     = element(by.xpath("//hx-tab[contains(text(),'Resources')]"));
 
