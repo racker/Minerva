b8fc3e27544c4ad907876bc20dd9a44c10693502
Lovedeep Singh <lovedeep.singh@rackspace.com>
2021-04-02 20:28:59 +0530

    fix xpath by replacing id

--- e2e/src/pages/adminToolsPage.ts
+++ e2e/src/pages/adminToolsPage.ts
@@ -2,7 +2,7 @@ import { element, by, browser, protractor } from "protractor";
 
 export class AdminToolsPage{
     
-    accountSearchBox = element(by.xpath("//input[@placeholder='Account Search']"));
+    accountSearchBox = element(by.id("demoSearch"));
     monitorsTab      = element(by.xpath("//hx-tab[contains(text(),'Monitors')]"));
     resourcesTab     = element(by.xpath("//hx-tab[contains(text(),'Resources')]"));
 
