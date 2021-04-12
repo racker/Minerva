0c21975660b67226960da0b8ce22105e8a600952
Lovedeep Singh <lovedeep.singh@rackspace.com>
2021-04-12 22:45:16 +0530

    add admin test case for getImpersonationToken function

--- projects/admin/src/app/app.component.ts
+++ projects/admin/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { ImpersonationService } from './_services/tenant/impersonation.service';
 import { TokenService } from './_services/auth/token.service';
 import { Router } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { impUser } from './_model/impersonationModel';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -18,13 +19,14 @@ export class AppComponent {
   name: string;
   xAuthToken : string;
   raxToken : string;
+  data : impUser;
 
   ngOnInit(): void {
     this.window['PORTAL_DATA'] = {isRacker:false, domainId: '7799042', tenants:['cloud:7799042']}; // TODO : tenantId (7799042) will be from account search input field.
   }
-  myClickFunction(event) {
+  myClickFunction() {
    
-    let data = {
+    this.data = {
       "RAX-AUTH:impersonation" : {
         user : {
           username : 'walterwhite'
@@ -34,7 +36,7 @@ export class AppComponent {
       // TODO : After tenant lookup, we got the racker token from an api or service called identity tokens and needs to be set into raxToken property.
 
     this.raxToken = 'AADU-8A-3pi8q4BIWfX2snQdcWEHye--dIEBnMD2MUX4I69hvt0lpx5W1P2E-jqIMtFmqslldMEtsxXUsl3P9pPhbjAohNON4PNQDrYyn7o7cpXIaiFyByEc';
-    this.impService.getImpersonationToken(data, this.raxToken)
+    this.impService.getImpersonationToken(this.data, this.raxToken)
         .subscribe(data => {
           this.xAuthToken = data['access'].token.id;
           this.tokenService.setToken = this.xAuthToken;
